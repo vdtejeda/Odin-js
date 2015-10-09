25c4f93f42aa0703e4191d18a0b7bc7492ea7726
Volney Tejeda <volneytejeda10@gmail.com>
2015-10-08 23:56:07 -0400

    add styling

--- Calculator/public/index.js
+++ Calculator/public/index.js
@@ -11,5 +11,12 @@ function add(num1, num2) {
   return num1 + num2;
 }
 function subtract(num1, num2) {
-  return um1 - num2;
+  return num1 - num2;
 }
+
+// var buttons = document.getElementsByClassName("button");
+// for (var i = 0, len = buttons.length; i < len; i++) {
+//   buttons[i].addEventListener("click", function(event) {
+//     handleInput(event.target.innerHTML);console.log(event.target.innerHTML);
+//   }, false);
+// }
